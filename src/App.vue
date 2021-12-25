edc86e5bbc8d46b8a18a5ef966756b333895ccf9
Rex <vivelarepublique@qq.com>
2021-12-26 03:54:19 +0800

    add almanac

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
 <div>
-  
+  <Almanac></Almanac>
 </div>
 
 </template>
 
 <script>
-
+import Almanac from './components/Almanac.vue';
 
 export default {
   name: 'App',
   components: {
-    
+    Almanac,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/background.js
+++ src/background.js
@@ -16,7 +16,7 @@ async function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      
+      webSecurity:false, /*同源策略*/
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,

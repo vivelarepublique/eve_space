3cf58af25fbacd1974fb68dabb1f67c0b4f1d771
Rex <vivelarepublique@qq.com>
2023-02-26 23:22:51 +0800

    Mineral update#1

--- src/pages/Mineral.vue
+++ src/pages/Mineral.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>开发中...</div>
+  <div class="container-fluid">
+    <div class="row">
+      <n-gradient-text class="col-md-3" type="info" size="24px"> 三钛合金 7.23 ISK, </n-gradient-text>
+      <n-gradient-text class="col-md-3" type="info" size="24px"> 类晶体胶矿 11.72 ISK </n-gradient-text>
+      <n-gradient-text class="col-md-3" type="info" size="24px"> 类银超金属 101.20 ISK </n-gradient-text>
+      <n-gradient-text class="col-md-3" type="info" size="24px"> 同位聚合体 412.30 ISK </n-gradient-text>
+      <n-gradient-text class="col-md-3" type="info" size="24px"> 超星诺克石 1,100.00 ISK </n-gradient-text>
+      <n-gradient-text class="col-md-3" type="info" size="24px"> 晶状石英岩 1,162.00 ISK</n-gradient-text>
+      <n-gradient-text class="col-md-3" type="info" size="24px"> 超噬矿 8,520.00 ISK </n-gradient-text>
+      <n-gradient-text class="col-md-3" type="info" size="24px"> 莫尔石 75,000.00 ISK </n-gradient-text>
+    </div>
+  </div>
 </template>
 
 <script>
+import { NGradientText } from 'naive-ui';
 export default {
   name: 'mineral',
+  components: {
+    NGradientText,
+  },
+  setup() {},
 };
 </script>
 
